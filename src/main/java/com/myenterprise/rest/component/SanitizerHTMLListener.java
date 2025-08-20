9a11fae895cffd8bb203ae31bb35de2d5f36c25c
cleanet <59420450+cleanet@users.noreply.github.com>
2025-08-20 23:18:31 +0200

    fix: license author

--- src/main/java/com/myenterprise/rest/component/SanitizerHTMLListener.java
+++ src/main/java/com/myenterprise/rest/component/SanitizerHTMLListener.java
@@ -1,7 +1,7 @@
 /*
  *   MIT License
  *
- *  Copyright (c) 2025 cleannet
+ *  Copyright (c) 2025 cleanet
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
