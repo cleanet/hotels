330e22180a6f608ed4e7a3f696f179c6a749d5a4
cleanet <59420450+cleanet@users.noreply.github.com>
2025-08-20 23:16:29 +0200

    docs: add license

--- src/main/java/com/myenterprise/rest/v1/configuration/SecurityConfiguration.java
+++ src/main/java/com/myenterprise/rest/v1/configuration/SecurityConfiguration.java
@@ -1,9 +1,25 @@
-/**
- * Security Configuration for allow to public access to Swagger UI and the API Documentation by OpenAPI.
- * <p>
- * This class defines a {@link SecurityFilterChain} for configure the HTTP authorization rules.
- * </p>
- * reference: https://www.baeldung.com/java-spring-security-permit-swagger-ui
+/*
+ *   MIT License
+ *
+ *  Copyright (c) 2025 cleannet
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  */
 package com.myenterprise.rest.v1.configuration;
 
@@ -24,6 +40,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
+ * Security Configuration for allow to public access to Swagger UI and the API Documentation by OpenAPI.
+ * <p>
+ * This class defines a {@link SecurityFilterChain} for configure the HTTP authorization rules.
+ * </p>
+ * reference: <a href="https://www.baeldung.com/java-spring-security-permit-swagger-ui">Spring Boot 3 – Configure Spring Security to Allow Swagger UI</a>
  * Main security configuration class for the application.
  * This class is responsible for setting up the Spring Security filter chain,
  * defining access control rules, and configuring Cross-Origin Resource Sharing (CORS).
